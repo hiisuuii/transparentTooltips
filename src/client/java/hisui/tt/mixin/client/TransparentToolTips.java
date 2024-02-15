99a3d4dbcbb016d466c69adae247520347c0fb75
hiisuuii <dogglez7777@gmail.com>
2024-02-15 14:37:25 -0700

    (annoying) specific port for 1.19.3

--- src/client/java/hisui/tt/mixin/client/TransparentToolTips.java
+++ src/client/java/hisui/tt/mixin/client/TransparentToolTips.java
@@ -2,20 +2,14 @@ package hisui.tt.mixin.client;
 
 import hisui.tt.duck.GameOptionsAccess;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.tooltip.TooltipBackgroundRenderer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.*;
 
-@Mixin(Screen.class)
+@Mixin(TooltipBackgroundRenderer.class)
 public abstract class TransparentToolTips {
-	@ModifyConstant(method = "renderTooltipFromComponents",
-			constant = @Constant(intValue = -267386864), slice = @Slice(
-					from = @At(value = "CONSTANT", args = "intValue=-267386864",ordinal = 5),
-			to = @At(value = "CONSTANT", args = "intValue=-267386864",ordinal = 6)
-	))
-	private int MoreTranslucentTooltipsMod$modifyTooltipBackgroundColor(int color) {
+	@ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/tooltip/TooltipBackgroundRenderer;renderRectangle(Lnet/minecraft/client/gui/tooltip/TooltipBackgroundRenderer$RectangleRenderer;Lorg/joml/Matrix4f;Lnet/minecraft/client/render/BufferBuilder;IIIIII)V"), index = 8)
+	private static int MoreTranslucentTooltipsMod$renderRectangleModifyArg(int color) {
 		var client = net.minecraft.client.MinecraftClient.getInstance();
 		var options = client.options;
 		int value = (int) (((GameOptionsAccess)options).transparentTooltips$getToolTipOpacity().getValue() * 255.0);
