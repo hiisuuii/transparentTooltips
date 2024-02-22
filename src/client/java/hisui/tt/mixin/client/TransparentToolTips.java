734c7619b47e2ac4cb4b3ea665797c9213f39396
hiisuuii <dogglez7777@gmail.com>
2024-02-22 14:25:36 -0700

    replace a brittle mixin

--- src/client/java/hisui/tt/mixin/client/TransparentToolTips.java
+++ src/client/java/hisui/tt/mixin/client/TransparentToolTips.java
@@ -1,20 +1,19 @@
 package hisui.tt.mixin.client;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import hisui.tt.duck.GameOptionsAccess;
 import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(Screen.class)
 public abstract class TransparentToolTips {
-	@ModifyConstant(method = "renderTooltipFromComponents",
-			constant = @Constant(intValue = -267386864), slice = @Slice(
-					from = @At(value = "CONSTANT", args = "intValue=-267386864",ordinal = 5),
-			to = @At(value = "CONSTANT", args = "intValue=-267386864",ordinal = 6)
-	))
+	@ModifyExpressionValue(method = "renderTooltipFromComponents",
+			at = @At(value = "CONSTANT", args = "intValue=-267386864"),
+			slice = @Slice(
+			from = @At(value = "CONSTANT", args = "intValue=-267386864",ordinal = 5),
+			to = @At(value = "CONSTANT", args = "intValue=-267386864",ordinal = 6)))
 	private int MoreTranslucentTooltipsMod$modifyTooltipBackgroundColor(int color) {
 		var client = net.minecraft.client.MinecraftClient.getInstance();
 		var options = client.options;
